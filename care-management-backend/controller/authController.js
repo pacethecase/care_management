e7eb22bbd8629e249a19b790e7c05bfe42a5cc21
Yami Naik <pacethecase123@gmail.com>
2025-12-03 14:38:51 +0530

    modified the envs for prod

--- care-management-backend/controller/authController.js
+++ care-management-backend/controller/authController.js
@@ -200,8 +200,8 @@ const login = async (req, res) => {
       // ✅ Set the token as httpOnly cookie
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "LAX",
+        secure: true,
+        sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000, 
       });
 
@@ -231,8 +231,8 @@ const login = async (req, res) => {
   const logout = (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
-        secure: false,
-        sameSite: "LAX",
+        secure: true,
+        sameSite: "None",
     });
     res.json({ message: "Logged out successfully" });
   };
